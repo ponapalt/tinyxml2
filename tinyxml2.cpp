5f2e70ec2b2eab4ac9aba4d34542e6bfc4f2a611
ponapalt <ponapalt@shillest.net>
2022-09-29 13:13:44 +0900

    last entry should be declaration **or comment**

--- tinyxml2.cpp
+++ tinyxml2.cpp
@@ -1151,7 +1151,7 @@ char* XMLNode::ParseDeep( char* p, StrPair* parentEndTag, int* curLineNumPtr )
                         FirstChild() &&
                         FirstChild()->ToDeclaration() &&
                         LastChild() &&
-                        LastChild()->ToDeclaration();
+                        ( LastChild()->ToDeclaration() || LastChild()->ToComment() ) ; //last entry should be declaration or comment.
                 }
                 else {
                     wellLocated = true;
